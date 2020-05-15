15a2680e28e33dd559fe7aee7fcb8a6a36a95a0b
HysunHe <hysunhe@foxmail.com>
2020-05-15 17:07:23 +0800

    test

--- custom/components/showWinningInfo.js
+++ custom/components/showWinningInfo.js
@@ -1,7 +1,7 @@
 'use strict';
-/*
+
 const logger = require('../../utils/Logger');
-const dbop = require('../../utils/dboperation');
+// const dbop = require('../../utils/dboperation');
 const moduleName = 'showWinningInfo';
 
 module.exports = {
@@ -17,21 +17,24 @@ module.exports = {
     const { user } = conversation.properties();
     logger.info(moduleName, "Got user: " + user);
 
-    let sqlConfig = {
-        "sql": "select WINNING_INFO FROM CEAIR_USERS where OPENID=:OPENID",
-        "bindParams": [user],
-        "options": {autoCommit: true},
-        "callback": function(result) {
-          let winInfo = '未找到您的最新中奖信息！';
-          if(result.rows.length > 0) {
-              winInfo = '您的最新中奖信息为 「' + result.rows[0][0] + '」。';
-          }
+    // let sqlConfig = {
+    //     "sql": "select WINNING_INFO FROM CEAIR_USERS where OPENID=:OPENID",
+    //     "bindParams": [user],
+    //     "options": {autoCommit: true},
+    //     "callback": function(result) {
+    //       let winInfo = '未找到您的最新中奖信息！';
+    //       if(result.rows.length > 0) {
+    //           winInfo = '您的最新中奖信息为 「' + result.rows[0][0] + '」。';
+    //       }
 
-          conversation.reply(`${winInfo}`).transition();
-          done();
-        }
-    }
-    dbop.exec(sqlConfig, logger);
+    //       conversation.reply(`${winInfo}`).transition();
+    //       done();
+    //     }
+    // }
+    // dbop.exec(sqlConfig, logger);
+
+    conversation.reply('Good').transition();
+    done();
   }
 };
-*/
+
