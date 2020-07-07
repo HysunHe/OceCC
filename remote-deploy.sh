250a2362ad06540687bfd760e54594f029413809
MW-Mac <mingyao.zhu@oracle.com>
2020-07-07 16:17:14 +0800

    update sh file

--- remote-deploy.sh
+++ remote-deploy.sh
@@ -1,5 +1,5 @@
 
 # Remote deployment
-#ssh -i /home/hysunhe/projects/BetterBot/credentials/sehub/id_rsa opc@o100.odainfra.com -t "sudo su - oracle -c ./deploy-ceair-connectors.sh"
+#ssh -i /home/hysunhe/projects/BetterBot/credentials/sehub/id_rsa opc@mileoda.hysun.cloudns.asia -t "sudo su - oracle -c ./deploy-ceair-connectors.sh"
 
-ssh -i /Users/mw-mac/Documents/ODA/ODA_CEC/id_rsa opc@oracle.doitchina.com -t "sudo su - oracle -c ./deploy-ocecc.sh"
+ssh -i /Users/mw-mac/Documents/ODA/ODA_CEC/id_rsa opc@mileoda.hysun.cloudns.asia -t "sudo su - oracle -c ./deploy-ocecc.sh"
